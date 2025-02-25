79480c813b26bff73fee7d2e7aa2de9eb4fb984a
Dinesh Budhathoki <physmarika@gmail.com>
2025-02-25 18:07:06 +0800

    Edit book feature is finished

--- src/models/book/bookModel.js
+++ src/models/book/bookModel.js
@@ -12,3 +12,13 @@ export const getAllPublicBooks = () => {
 export const getAllBooks = (filter) => {
   return bookSchema.find(filter);
 };
+
+//this is for updating the book
+export const updateBook = ({ _id, ...rest }) => {
+  return bookSchema.findByIdAndUpdate(_id, rest);
+};
+
+//this is for deleting the book
+export const deleteBook = (_id) => {
+  return bookSchema.findByIdAndDelete(_id);
+};
